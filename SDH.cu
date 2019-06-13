e0d8563c7bb1550cb8c31673b4ae6c8e42d3bae8
tj <tanmay@mail.usf.edu>
2019-06-13 14:22:55 -0400

    working on project 2 code now.. step 1: get the tiling algorithm to work

--- SDH.cu
+++ SDH.cu
@@ -339,7 +339,7 @@ int main(int argc, char **argv)
 	//run the kernel
 	// PDH_kernel<<<ceil(PDH_acnt/256.0), 256>>>(d_gpu_histogram, d_atom_list, PDH_acnt, PDH_res);
 	// PDH_kernel<<<ceil(PDH_acnt/((float)blockcount)), blockcount>>>(d_gpu_histogram, d_atom_x_list, d_atom_y_list, d_atom_z_list, PDH_acnt, PDH_res);
-	PDH_kernel2<<<ceil(PDH_acnt/((float(blockcount))), blockcount>>>(d_gpu_histogram, d_atom_x_list, d_atom_y_list, d_atom_z_list, PDH_acnt, PDH_res, blockcount);
+	PDH_kernel2<<<ceil(PDH_acnt/(((float)blockcount))), blockcount>>>(d_gpu_histogram, d_atom_x_list, d_atom_y_list, d_atom_z_list, PDH_acnt, PDH_res, blockcount);
 
 	//copy the histogram results back from gpu over to cpu
 	cudaMemcpy(h_gpu_histogram, d_gpu_histogram, sizeof(bucket)*num_buckets, cudaMemcpyDeviceToHost);
