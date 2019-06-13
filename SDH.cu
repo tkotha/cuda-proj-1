7c2aba1e179951d1815aaab4d076e3cbb620d606
tj <tanmay@mail.usf.edu>
2019-06-13 17:52:33 -0400

    working on project 2 code now.. step 1: get the tiling algorithm to work

--- SDH.cu
+++ SDH.cu
@@ -186,18 +186,26 @@ __global__ void PDH_kernel2(unsigned long long* d_histogram,
 
 		for(i = b+1; i < M; i++)
 		{
-			R[t + BLOCK_SIZE*0] = d_atom_x_list[t + i*B];
-			R[t + BLOCK_SIZE*1] = d_atom_y_list[t + i*B];
-			R[t + BLOCK_SIZE*2] = d_atom_z_list[t + i*B];
+			// R[t + BLOCK_SIZE*0] = d_atom_x_list[t + i*B];
+			// R[t + BLOCK_SIZE*1] = d_atom_y_list[t + i*B];
+			// R[t + BLOCK_SIZE*2] = d_atom_z_list[t + i*B];
 				
+			R[3*t + 0] = d_atom_x_list[t + i*B];
+			R[3*t + 1] = d_atom_y_list[t + i*B];
+			R[3*t + 2] = d_atom_z_list[t + i*B];
+
 			__syncthreads();
 
 			for(j = 0; j < B; j++)
 			{
 				
-				x2 = R[j + BLOCK_SIZE*0];
-				y2 = R[j + BLOCK_SIZE*1];
-				z2 = R[j + BLOCK_SIZE*2];
+				// x2 = R[j + BLOCK_SIZE*0];
+				// y2 = R[j + BLOCK_SIZE*1];
+				// z2 = R[j + BLOCK_SIZE*2];
+
+				x2 = R[3*j + 0];
+				y2 = R[3*j + 1];
+				z2 = R[3*j + 2];
 				d = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
 				h_pos = (int) (d/res);
 				// atomicAdd(&d_histogram[h_pos].d_cnt, 1);
@@ -208,16 +216,23 @@ __global__ void PDH_kernel2(unsigned long long* d_histogram,
 			
 		}
 
-		R[t + BLOCK_SIZE*0] = d_atom_x_list[reg];
-		R[t + BLOCK_SIZE*1] = d_atom_y_list[reg];
-		R[t + BLOCK_SIZE*2] = d_atom_z_list[reg];
+		// R[t + BLOCK_SIZE*0] = d_atom_x_list[reg];
+		// R[t + BLOCK_SIZE*1] = d_atom_y_list[reg];
+		// R[t + BLOCK_SIZE*2] = d_atom_z_list[reg];
+		R[3*t + 0] = d_atom_x_list[reg];
+		R[3*t + 1] = d_atom_y_list[reg];
+		R[3*t + 2] = d_atom_z_list[reg];
 		__syncthreads();
 
 		for(i = t+1; i < B; i++)
 		{
-			x2 = R[i + BLOCK_SIZE*0];
-			y2 = R[i + BLOCK_SIZE*1];
-			z2 = R[i + BLOCK_SIZE*2];
+			// x2 = R[i + BLOCK_SIZE*0];
+			// y2 = R[i + BLOCK_SIZE*1];
+			// z2 = R[i + BLOCK_SIZE*2];
+
+			x2 = R[3*i + 0];
+			y2 = R[3*i + 1];	
+			z2 = R[3*i + 2];	
 
 			d = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
 			h_pos = (int) (d/res);
