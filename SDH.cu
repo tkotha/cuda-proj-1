fbbe1689bf4220807711c52a3806cd553ebc98ec
tj <tanmay@mail.usf.edu>
2019-06-13 17:48:31 -0400

    working on project 2 code now.. step 1: get the tiling algorithm to work

--- SDH.cu
+++ SDH.cu
@@ -177,53 +177,54 @@ __global__ void PDH_kernel2(unsigned long long* d_histogram,
 							//the z array should be accessed by R[t + BLOCK_SIZE*2] //or is it R[t*3 + 2]?
 
 	//make sure we are a valid atom in the array
-	if(reg > acnt) return;
-
-
-	x1 = d_atom_x_list[reg];
-	y1 = d_atom_y_list[reg];
-	z1 = d_atom_z_list[reg];
-
-	for(i = b+1; i < M; i++)
+	if(reg < acnt) 
 	{
-		R[t + BLOCK_SIZE*0] = d_atom_x_list[t + i*B];
-		R[t + BLOCK_SIZE*1] = d_atom_y_list[t + i*B];
-		R[t + BLOCK_SIZE*2] = d_atom_z_list[t + i*B];
+
+		x1 = d_atom_x_list[reg];
+		y1 = d_atom_y_list[reg];
+		z1 = d_atom_z_list[reg];
+
+		for(i = b+1; i < M; i++)
+		{
+			R[t + BLOCK_SIZE*0] = d_atom_x_list[t + i*B];
+			R[t + BLOCK_SIZE*1] = d_atom_y_list[t + i*B];
+			R[t + BLOCK_SIZE*2] = d_atom_z_list[t + i*B];
+				
+			__syncthreads();
+
+			for(j = 0; j < B; j++)
+			{
+				
+				x2 = R[j + BLOCK_SIZE*0];
+				y2 = R[j + BLOCK_SIZE*1];
+				z2 = R[j + BLOCK_SIZE*2];
+				d = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
+				h_pos = (int) (d/res);
+				// atomicAdd(&d_histogram[h_pos].d_cnt, 1);
+				atomicAdd(&d_histogram[h_pos], 1);
 			
+			}
+			__syncthreads();
+			
+		}
+
+		R[t + BLOCK_SIZE*0] = d_atom_x_list[reg];
+		R[t + BLOCK_SIZE*1] = d_atom_y_list[reg];
+		R[t + BLOCK_SIZE*2] = d_atom_z_list[reg];
 		__syncthreads();
 
-		for(j = 0; j < B; j++)
+		for(i = t+1; i < B; i++)
 		{
-			
-			x2 = R[j + BLOCK_SIZE*0];
-			y2 = R[j + BLOCK_SIZE*1];
-			z2 = R[j + BLOCK_SIZE*2];
+			x2 = R[i + BLOCK_SIZE*0];
+			y2 = R[i + BLOCK_SIZE*1];
+			z2 = R[i + BLOCK_SIZE*2];
+
 			d = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
 			h_pos = (int) (d/res);
-			// atomicAdd(&d_histogram[h_pos].d_cnt, 1);
 			atomicAdd(&d_histogram[h_pos], 1);
-		
 		}
 		__syncthreads();
-		
 	}
-
-	R[t + BLOCK_SIZE*0] = d_atom_x_list[reg];
-	R[t + BLOCK_SIZE*1] = d_atom_y_list[reg];
-	R[t + BLOCK_SIZE*2] = d_atom_z_list[reg];
-	__syncthreads();
-
-	for(i = t+1; i < B; i++)
-	{
-		x2 = R[i + BLOCK_SIZE*0];
-		y2 = R[i + BLOCK_SIZE*1];
-		z2 = R[i + BLOCK_SIZE*2];
-
-		d = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
-		h_pos = (int) (d/res);
-		atomicAdd(&d_histogram[h_pos], 1);
-	}
-	__syncthreads();
 }
 
 /* 
@@ -346,12 +347,12 @@ int main(int argc, char **argv)
 	printf("shmemsize:  %d\n", shmemsize);
 	//run the kernel
 	// PDH_kernel<<<ceil(PDH_acnt/256.0), 256>>>(d_gpu_histogram, d_atom_list, PDH_acnt, PDH_res);
-	PDH_kernel<<<blockcount, BLOCK_SIZE>>>(d_gpu_histogram, d_atom_x_list, d_atom_y_list, d_atom_z_list, PDH_acnt, PDH_res);
-	// PDH_kernel2<<<blockcount, BLOCK_SIZE, shmemsize>>>
-	// (d_gpu_histogram, 
-	// 	d_atom_x_list, d_atom_y_list, d_atom_z_list, 
-	// 	PDH_acnt, PDH_res,
-	// 	 blockcount, BLOCK_SIZE);
+	// PDH_kernel<<<blockcount, BLOCK_SIZE>>>(d_gpu_histogram, d_atom_x_list, d_atom_y_list, d_atom_z_list, PDH_acnt, PDH_res);
+	PDH_kernel2<<<blockcount, BLOCK_SIZE, shmemsize>>>
+	(d_gpu_histogram, 
+		d_atom_x_list, d_atom_y_list, d_atom_z_list, 
+		PDH_acnt, PDH_res,
+		 blockcount, BLOCK_SIZE);
 
 	//copy the histogram results back from gpu over to cpu
 	cudaMemcpy(h_gpu_histogram, d_gpu_histogram, sizeof(unsigned long long)*num_buckets, cudaMemcpyDeviceToHost);
