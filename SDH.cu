a18611b69bcaa22d0f7c5d803c2dae62eb333056
tj <tanmay@mail.usf.edu>
2019-06-18 13:36:50 -0400

    project 2 2nd attempt.. step 1: get the tiling algorithm to work

--- SDH.cu
+++ SDH.cu
@@ -524,17 +524,12 @@ int main(int argc, char **argv)
 
 	//run the kernel
 
-	// PDH_kernel<<<blockcount, BLOCK_SIZE>>>(d_gpu_histogram, d_atom_x_list, d_atom_y_list, d_atom_z_list, PDH_acnt, PDH_res);
-	// PDH_kernel2 <<<blockcount, BLOCK_SIZE, 2*shmemsize>>> //for now, we're allocating blocks for both L and R
-	// 	(d_gpu_histogram, 
-	// 	 d_atom_x_list, d_atom_y_list, d_atom_z_list, 
-	// 	 PDH_acnt, PDH_res,
-	// 	 blockcount, BLOCK_SIZE);
+	PDH_kernel<<<blockcount, BLOCK_SIZE>>>(d_gpu_histogram, d_atom_x_list, d_atom_y_list, d_atom_z_list, PDH_acnt, PDH_res);
 
-	PDH_kernel3 <<<blockcount, BLOCK_SIZE, shmemsize3>>> //now we try and use just R
-	(d_gpu_histogram, 
-		d_atom_x_list, d_atom_y_list, d_atom_z_list, 
-		PDH_acnt, PDH_res);
+	// PDH_kernel3 <<<blockcount, BLOCK_SIZE, shmemsize3>>> //now we try and use just R
+	// (d_gpu_histogram, 
+	// 	d_atom_x_list, d_atom_y_list, d_atom_z_list, 
+	// 	PDH_acnt, PDH_res);
 
 	// PDH_kernel4 <<<blockcount, BLOCK_SIZE, shmemsize4>>> //now we try to privatize the histogram
 	// (d_gpu_histogram, 
