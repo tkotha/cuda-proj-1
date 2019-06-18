5c97c1f8bf7441c97f730544f0624b23730e49e8
tj <tanmay@mail.usf.edu>
2019-06-18 10:41:33 -0400

    project 2 2nd attempt.. step 1: get the tiling algorithm to work

--- SDH.cu
+++ SDH.cu
@@ -367,7 +367,7 @@ __global__ void PDH_kernel4(unsigned long long* d_histogram,
 	__syncthreads();
 	for(i = t; i < histSize*numHists; i += blockDim.x)
 	{
-		atomicAdd(&d_histogram[i], sh_hist[i]);
+		atomicAdd(&d_histogram[i % numHists], sh_hist[i]);
 	}
 
 }
