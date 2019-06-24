f49eb82b0dd0955b3aef8d45457bb9db5660311b
tj <tanmay@mail.usf.edu>
2019-06-24 17:28:34 -0400

    project 2 3rd attempt.. get the histogram priv to be accurate

--- SDH.cu
+++ SDH.cu
@@ -373,9 +373,9 @@ __global__ void PDH_kernel4(unsigned long long* d_histogram,
 				// Rx = R[i];
 				// Ry = R[i + blockDim.x];
 				// Rz = R[i + blockDim.x*2];
-				int Rxi = __shfl_down_sync (0xffffffff, j, i, 32);
-				int Ryi = __shfl_down_sync (0xffffffff, j, i, 32);
-				int Rzi = __shfl_down_sync (0xffffffff, j, i, 32);
+				int Rxi = __shfl_down (0xffffffff, j, i, 32);
+				int Ryi = __shfl_down (0xffffffff, j, i, 32);
+				int Rzi = __shfl_down (0xffffffff, j, i, 32);
 				dist = sqrt((Lx - Rx)*(Lx-Rx) + (Ly - Ry)*(Ly - Ry) + (Lz - Rz)*(Lz - Rz));
 				/* END DISTANCE FUNCTION */
 
