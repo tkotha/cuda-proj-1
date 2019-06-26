86cbea4855490c2d8e35f8b7b622a2fbb68ac6dc
tj <tanmay@mail.usf.edu>
2019-06-26 15:06:06 -0400

    project 2 3rd attempt.. get the histogram priv to be accurate

--- SDH.cu
+++ SDH.cu
@@ -13,13 +13,13 @@
 
 #define BOX_SIZE	23000 /* size of the data box on one dimension            */
 #define COMPARE_CPU 1
-#define KERNELTYPE 1
+#define KERNELTYPE 3
 
-// #define ATOM_DIM double
-// #define ATOM_ZERO 0.0
+#define ATOM_DIM double
+#define ATOM_ZERO 0.0
 
-#define ATOM_DIM float
-#define ATOM_ZERO 0
+// #define ATOM_DIM float
+// #define ATOM_ZERO 0
 
 
 /* descriptors for single atom in the tree */
@@ -136,7 +136,7 @@ __global__ void PDH_kernel(unsigned long long* d_histogram,
 {
 	int id = blockIdx.x*blockDim.x + threadIdx.x;
 	int j, h_pos;
-	double dist;
+	ATOM_DIM dist;
 	ATOM_DIM x1;
 	ATOM_DIM x2;
 	ATOM_DIM y1;
@@ -152,8 +152,8 @@ __global__ void PDH_kernel(unsigned long long* d_histogram,
 			y2 = d_atom_y_list[j];
 			z1 = d_atom_z_list[id];
 			z2 = d_atom_z_list[j];
-			dist = sqrt( (double) ( (x1 - x2)*(x1-x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2) ) );
-			h_pos = (int) ((float)dist / res);
+			dist = sqrt((x1 - x2)*(x1-x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2));
+			h_pos = (int) (dist / res);
 			// atomicAdd((unsigned long long int*)&d_histogram[h_pos].d_cnt,1);
 			atomicAdd(&d_histogram[h_pos], 1);
 		}
