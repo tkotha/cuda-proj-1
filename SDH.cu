679f751cc2184395f60c3a67055d4257b26aeabf
tj <tanmay@mail.usf.edu>
2019-06-26 16:35:07 -0400

    project 2 3rd attempt.. get the histogram priv to be accurate

--- SDH.cu
+++ SDH.cu
@@ -15,13 +15,13 @@
 #define COMPARE_CPU 1
 #define KERNELTYPE 4
 
-// #define ATOM_DIM double
-// #define ATOM_ZERO 0.0
-// #define SQRT sqrt
-
-#define ATOM_DIM float
+#define ATOM_DIM double
 #define ATOM_ZERO 0.0
-#define SQRT sqrtf
+#define SQRT sqrt
+
+// #define ATOM_DIM float
+// #define ATOM_ZERO 0.0
+// #define SQRT sqrtf
 
 
 /* descriptors for single atom in the tree */
