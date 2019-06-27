4487a60f832e1df5aaee451822bb32d47f0b9225
tj <tanmay@mail.usf.edu>
2019-06-27 14:58:59 -0400

    project 2 3rd attempt.. get the histogram priv to be accurate

--- SDH.cu
+++ SDH.cu
@@ -545,18 +545,25 @@ void output_histogram(unsigned long long* histogram){
 		else printf("| ");
 	}
 }
-
+int isNumber(char const*arg)
+{
+	int n;
+	return (sscanf(arg, "%d", &n) == 1);
+}
 
 int main(int argc, char **argv)
 {
 	int i;
 	// #define BLOCK_SIZE 64 /*This is temporary until I can a) make sure the basic algorithm is correct and b)I've made sure i know how to dynamically allocate shared memory
-	if(argc != 4)
+	if(argc != 4 || !isNumber(argv[1]) || !isNumber(arg[3]))
 	{
 		printf("ERROR: Must specify 3 arguments after ./SDH! parameters allowed are (in order):\n sample_num(int), bucket_width(double), block_size(int)\n");
 		printf("Example Execution: ./SDH 10000 500.0 64\n This will compute 10000 points,\n using histogram with bucket resolution 500.0,\n using 64 blocks in the GPU\n");
 		return 1;
 	}
+
+
+
 	PDH_acnt = atoi(argv[1]);
 	PDH_res	 = atof(argv[2]);
 	BLOCK_SIZE = atoi(argv[3]);
