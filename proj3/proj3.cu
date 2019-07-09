583f49a8ed245bfe3e5b542019832492ae45961d
tj <tanmay@mail.usf.edu>
2019-07-09 11:42:25 -0400

    project3 1st attempt

--- proj3/proj3.cu
+++ proj3/proj3.cu
@@ -72,6 +72,8 @@ __global__ void histogram(int* i_r_h, int i_rh_size, int i_numPartitions ,int* o
 //implement it yourself or borrow the code from CUDA samples
 //so it seems the width is based on numPartitions
 //remember that this is exclusive scan
+
+//does this work....? iunno.... but it seems to not crash on input size of 10... which is a start i guess
 __global__ void prefixScan(int* i_histogram, int* o_prefix_sum)
 {
     //maybe i need multiple device kernels for this to work...
@@ -216,14 +218,14 @@ int main(int argc, char *argv[])
     //also we dont really know how to deal with offsets?
     //is that just printing out the histogram sizes?
     //does pointer offset in this case simply mean index of current partition into the reordered array?
-    int currentSum = 0;
+    // int currentSum = 0;
     for(int i = 0; i < numPartitions; i++)
     {
         int curval = h_histogram[i];
         printf("Partition %d:\n", i+1);
-        printf("    Pointer offset:    %d\n", currentSum);
+        printf("    Pointer offset:    %d\n", prefix_sum[i]);
         printf("    Number of Keys: %d\n", curval);
-        currentSum += curval;
+        // currentSum += curval;
     }
     printf("\n");
 
